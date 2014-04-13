198bbca88f5775a4d32b1ba52bdf61e45efa7605
Doridian Draconia <mriq91@gmail.com>
2014-04-13 02:39:06 +0200

    Add Buckets

--- src/main/java/de/doridian/logstashlogger/LoggerListener.java
+++ src/main/java/de/doridian/logstashlogger/LoggerListener.java
@@ -13,6 +13,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -21,33 +22,46 @@ public class LoggerListener implements Listener {
 		RedisQueueThread.queueAction(new PlayerBlockAction(user, location, materialBefore, materialAfter));
 	}
 
-	@EventHandler(priority = EventPriority.MONITOR)
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onBlockPlace(BlockPlaceEvent event) {
-		if(event.isCancelled())
-			return;
-
 		addBlockChange(event.getPlayer(), event.getBlockPlaced().getLocation(), event.getBlockReplacedState().getType(), event.getBlockPlaced().getType());
 	}
 
-	@EventHandler(priority = EventPriority.MONITOR)
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onBlockBreak(BlockBreakEvent event) {
-		if(event.isCancelled())
-			return;
-
 		addBlockChange(event.getPlayer(), event.getBlock().getLocation(), event.getBlock().getType(), Material.AIR);
 	}
 
-	@EventHandler(priority = EventPriority.MONITOR)
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onBucketFill(PlayerBucketFillEvent event) {
+		addBlockChange(event.getPlayer(), event.getBlockClicked().getLocation(), event.getBlockClicked().getType(), Material.AIR);
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onBucketEmpty(PlayerBucketFillEvent event) {
+		Material material = event.getBucket();
+		switch(event.getBucket()) {
+			case WATER_BUCKET:
+				material = Material.WATER;
+				break;
+			case LAVA_BUCKET:
+				material = Material.LAVA;
+				break;
+		}
+		addBlockChange(event.getPlayer(), event.getBlockClicked().getRelative(event.getBlockFace()).getLocation(), Material.AIR, material);
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		RedisQueueThread.queueAction(new PlayerAction(event.getPlayer(), "join"));
 	}
 
-	@EventHandler(priority = EventPriority.MONITOR)
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		RedisQueueThread.queueAction(new PlayerAction(event.getPlayer(), "quit"));
 	}
 
-	@EventHandler(priority = EventPriority.MONITOR)
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerChat(AsyncPlayerChatEvent event) {
 		RedisQueueThread.queueAction(new PlayerChatAction(event.getPlayer(), event.getMessage()));
 	}
